407c0bc9fb2caf544d630058f2a31a190b4d23a3
DanielRodado <d4nielrodado@gmail.com>
2024-08-27 18:29:05 -0500

    Anotación @Bean agregada

--- src/main/java/com/example/userservice/Utils.java
+++ src/main/java/com/example/userservice/Utils.java
@@ -3,15 +3,17 @@ package com.example.userservice;
 import com.example.userservice.models.UserEntity;
 import com.example.userservice.repositories.UserEntityRepository;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class Utils {
 
+    @Bean
     public CommandLineRunner initData(UserEntityRepository userEntityRepository) {
 
         return args -> {
-            UserEntity user1 = new UserEntity("Daniel", "daniel@example.com", "12345");
+            /*UserEntity user1 = new UserEntity("Daniel", "daniel@example.com", "12345");
             userEntityRepository.save(user1).subscribe();
 
             UserEntity user2 = new UserEntity("Maria", "maria@example.com", "12345");
@@ -21,7 +23,7 @@ public class Utils {
             userEntityRepository.save(user3).subscribe();
 
             UserEntity user4 = new UserEntity("Carlos", "carlos@example.com", "12345");
-            userEntityRepository.save(user4).subscribe();
+            userEntityRepository.save(user4).subscribe();*/
         };
     }
 

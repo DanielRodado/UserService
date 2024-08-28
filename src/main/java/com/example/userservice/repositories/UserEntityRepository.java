b3bd20c169e523de376c24f651ef7ecccb35c1c4
DanielRodado <d4nielrodado@gmail.com>
2024-08-27 21:08:49 -0500

    Método para buscar por un 'UserEntity' por email

--- src/main/java/com/example/userservice/repositories/UserEntityRepository.java
+++ src/main/java/com/example/userservice/repositories/UserEntityRepository.java
@@ -2,6 +2,10 @@ package com.example.userservice.repositories;
 
 import com.example.userservice.models.UserEntity;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import reactor.core.publisher.Mono;
 
 public interface UserEntityRepository extends ReactiveCrudRepository<UserEntity, Long> {
+
+    Mono<UserEntity> findByEmail(String email);
+
 }

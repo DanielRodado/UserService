b236c15f138bd636801f7432d2f81b1e99d46361
DanielRodado <d4nielrodado@gmail.com>
2024-08-27 21:12:01 -0500

    Mappers (Mono & Flux)

---
+++ src/main/java/com/example/userservice/mappers/UserEntityMapper.java
@@ -0,0 +1,27 @@
+package com.example.userservice.mappers;
+
+import com.example.userservice.dto.UserApplicationDTO;
+import com.example.userservice.dto.UserEntityDTO;
+import com.example.userservice.models.UserEntity;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+public class UserEntityMapper {
+
+    // To DTO
+
+    public static Mono<UserEntityDTO> toUserEntityDTOMono(Mono<UserEntity> userEntityMono) {
+        return userEntityMono.map(UserEntityDTO::new);
+    }
+
+    public static Flux<UserEntityDTO> toUserEntityDTOFlux(Flux<UserEntity> userEntityFlux) {
+        return userEntityFlux.map(UserEntityDTO::new);
+    }
+
+    // To Entity
+
+    public static Mono<UserEntity> toUserEntityMono(Mono<UserApplicationDTO> userAppMono) {
+        return userAppMono.map(userApplicationDTO -> new UserEntity(userApplicationDTO.name(), userApplicationDTO.email(), userApplicationDTO.password()));
+    }
+
+}

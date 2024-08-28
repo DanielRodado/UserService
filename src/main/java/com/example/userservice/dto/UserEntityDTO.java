7111a20df5f84c17ce6ba78ff6c361aaade7e463
DanielRodado <d4nielrodado@gmail.com>
2024-08-28 14:08:49 -0500

    Se le agregó la propiedad id al DTO

--- src/main/java/com/example/userservice/dto/UserEntityDTO.java
+++ src/main/java/com/example/userservice/dto/UserEntityDTO.java
@@ -4,11 +4,17 @@ import com.example.userservice.models.UserEntity;
 
 public class UserEntityDTO {
 
+    private final Long id;
     private final String name, email;
 
     public UserEntityDTO(UserEntity userEntity) {
+        this.id = userEntity.getId();
         this.name = userEntity.getName();
         this.email = userEntity.getEmail();
+    }
+
+    public Long getId() {
+        return id;
     }
 
     public String getName() {

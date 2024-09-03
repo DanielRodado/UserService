4783748415b211c04265b13975fb405f851f94b2
DanielRodado <d4nielrodado@gmail.com>
2024-09-02 21:26:06 -0500

    Configuraciones de springdoc

--- src/main/java/com/example/userservice/configurations/OpenApiConfig.java
+++ src/main/java/com/example/userservice/configurations/OpenApiConfig.java
@@ -5,6 +5,10 @@ import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@OpenAPIDefinition(info = @Info(title = "UserService APP", version = "V1"))
+@OpenAPIDefinition(info = @Info(
+        title = "UserService APP",
+        version = "V1",
+        description = "API for managing users in the UserService application."
+))
 public class OpenApiConfig {
 }

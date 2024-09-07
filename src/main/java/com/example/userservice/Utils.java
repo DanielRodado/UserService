11d1600e4a35e259b549e4432267ea8ca21d5bda
DanielRodado <d4nielrodado@gmail.com>
2024-09-06 20:50:09 -0500

    PasswordEncoder agregado a los datos de prueba

--- src/main/java/com/example/userservice/Utils.java
+++ src/main/java/com/example/userservice/Utils.java
@@ -1,27 +1,36 @@
 package com.example.userservice;
 
 import com.example.userservice.repositories.UserEntityRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class Utils {
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     @Bean
     public CommandLineRunner initData(UserEntityRepository userEntityRepository) {
 
         return args -> {
-            /*UserEntity user1 = new UserEntity("Daniel", "daniel14","daniel@example.com", "12345", true);
+            /*UserEntity user1 = new UserEntity("Daniel", "daniel14","daniel@example.com", passwordEncoder.encode(
+                    "12345"), true);
             userEntityRepository.save(user1).subscribe();
 
-            UserEntity user2 = new UserEntity("Stephany", "Steph23","steph@example.com", "12345", true);
+            UserEntity user2 = new UserEntity("Stephany", "Steph23","steph@example.com", passwordEncoder.encode(
+                    "12345"), true);
             userEntityRepository.save(user2).subscribe();
 
-            UserEntity user3 = new UserEntity("Pedro", "prezzz12","pedro@example.com", "12345");
+            UserEntity user3 = new UserEntity("Pedro", "prezzz12","pedro@example.com", passwordEncoder.encode(
+                    "12345"));
             userEntityRepository.save(user3).subscribe();
 
-            UserEntity user4 = new UserEntity("Carlos", "carlo21","carlos@example.com", "12345");
+            UserEntity user4 = new UserEntity("Carlos", "carlo21","carlos@example.com", passwordEncoder.encode(
+                    "12345"));
             userEntityRepository.save(user4).subscribe();*/
         };
     }
